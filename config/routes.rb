9d66c36ba7f460dbee0304849e0afe4411903263
Lucas Rocha <lrocha5555@gmail.com>
2022-08-05 16:45:07 -0300

    Creating a about page and setting up the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#home"
 
+  get "about", to: "about#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
