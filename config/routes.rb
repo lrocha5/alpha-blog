7cc3ce792a9b4ecc88456be9232b1e4cebad0b86
Lucas Rocha <lrocha5555@gmail.com>
2022-08-06 16:14:15 -0300

    Create a Article Model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "pages#home"
 
-  get "about", to: "about#index"
+  get "about", to: "pages#about"
+
+  resources :articles
   # Defines the root path route ("/")
   # root "articles#index"
 end
