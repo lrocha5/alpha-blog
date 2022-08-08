06d46b918e2e0dd70a13b2d488548e1ba12fe5e8
Lucas Rocha <lrocha5555@gmail.com>
2022-08-08 14:09:33 -0300

    Create a '_form' partial, refactor the 'new article' page and 'edit article' page.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    
     def show
-        @article = Article.find(params[:id])
+
     end
 
     def index
@@ -12,7 +14,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         
         
         if @article.save
@@ -27,13 +29,13 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+
     end
 
     def update
-        @article = Article.find(params[:id])
 
-        if @article.update(params.require(:article).permit(:title, :description))
+
+        if @article.update(article_params)
             flash[ :notice ] = "Article was updated successfully."
             redirect_to @article
         else
@@ -43,9 +45,20 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
+
         @article.destroy
         redirect_to articles_path
+    end
+
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 
 end
